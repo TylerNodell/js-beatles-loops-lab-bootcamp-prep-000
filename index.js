e4b9d8742bd10f040f71b49f0e788932b7381e08
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-25 19:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function theBeatlesPlay(am, ai) {
   var blank = []
   for (var i = 0; i < am.length - 1; i++) {
-    var name = am[i]
-    var inst = ai[i]
     blank.push(`${am[i]} plays the ${ai[i]}`)
   }
   return blank
+}
+
+function johnLennonFacts(arg) {
+  var array = []
+  array = array.push(arg)
+  while (i < array.length()) {
+    array[i] = array[i] + "!!!"
+    i++
+  }
 }
